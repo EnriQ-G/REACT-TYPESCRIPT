ca4f87c61a6f2ca71e32b98a8e9311bd2a895ef6
Enrique Graniel <legraniel.91@gmail.com>
2024-01-24 17:43:01 -0600

    cambios al proceso de creación del array

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  const [subs, setSubs] = useState([
+  const initial_state = [
     {
       nick: 'dapelu',
       subMonths: 3,
@@ -14,8 +13,21 @@ function App() {
       subMonths: 4,
       avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
     }
-  ])
+  ]
 
+  interface Sub {
+    nick: string
+    avatar: string
+    subMonths: number
+    description?: string
+  }
+
+function App() {
+  const [subs, setSubs] = useState<Array<Sub>>([])
+  
+  useEffect(() =>{
+    setSubs(initial_state)
+  }, [])
 
 
   return (
